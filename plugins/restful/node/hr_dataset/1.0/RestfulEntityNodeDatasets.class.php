0f57f8b6716b7411efb6c9dcb1f13284ccd97302
Guillaume Viguier <guillaume@viguierjust.com>
2015-06-22 10:00:36 +0200

    Added created and changed timestamps

--- plugins/restful/node/hr_dataset/1.0/RestfulEntityNodeDatasets.class.php
+++ plugins/restful/node/hr_dataset/1.0/RestfulEntityNodeDatasets.class.php
@@ -106,6 +106,14 @@ class RestfulEntityNodeDatasets extends \RestfulEntityBaseNode {
       'process_callbacks' => array(array($this, 'getEntity')),
     );
 
+    $public_fields['created'] = array(
+      'property' => 'created',
+    );
+
+    $public_fields['changed'] = array(
+      'property' => 'changed',
+    );
+
     $public_fields['url'] = array(
       'property' => 'url',
     );
